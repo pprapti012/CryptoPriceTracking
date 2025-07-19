b069e6897bf9bf2a75dc28b5cf3c07f9b319bc81
Prapti Patil <pprapti012@gmail.com>
2025-07-19 21:09:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import NavBar from "./components/NavBar/NavBar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/home";
+import Coin from "./pages/Coin/Coin";
+import Footer from "./components/Footer/Footer";
+
+const App = () => {
+  return (
+    <div className="app">
+      <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+<Route path="/coin/:coinId" element={<Coin />} />
+          </Routes>
+      <Footer/>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/context/CoinContext.jsx
@@ -0,0 +1,58 @@
+import { createContext, useEffect, useState } from "react";
+
+export const CoinContext = createContext();
+
+
+const CoinContextProvider = ({children}) => {
+  const [allCoin, setAllcoin] = useState([]);
+  const [currency, setCurrency] = useState({
+    name: "usd",
+    symbol: "$",
+  });
+
+
+
+  const fetchAllCoin = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": "CG-1hd27Xe4YiEZzoeHJqSpnR2q",
+      },
+    };
+
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setAllcoin(res))
+      .catch((err) => console.error(err));
+  };
+
+
+
+  useEffect(() => {
+    fetchAllCoin();
+  }, [currency]);
+
+
+
+  const contextValue = {
+    allCoin,
+    currency,
+    setCurrency,
+  };
+
+
+
+  return (
+  <CoinContext.Provider value={contextValue}>
+    {children}
+  </CoinContext.Provider>
+);
+};
+
+
+
+export default CoinContextProvider;
